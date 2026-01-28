f4d2894847abefe3ddf0d1d49c320b6952dfe521
raghda alnjjar <raghdaiyadalnjjar@gmail.com>
2026-01-28 10:59:11 +0200

    Fix: Change API URL to HTTPS to resolve Mixed Content error

--- src/hooks/useMovies.jsx
+++ src/hooks/useMovies.jsx
@@ -14,7 +14,7 @@ export function useMovies(query) {
           setIsLoading(true);
           setIsError("");
           const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+            `https://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
             { signal: controller.signal },
           );
 
